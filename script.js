8310e7325d82c9f5db0a9b948b164b769ecaf833
Sakima228 <151528810+Sakima228@users.noreply.github.com>
2023-12-05 13:36:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var car = ["kia","bmw",];
+var age = [2000,1998,2005];
+var el = document.getElementById("age");
+if (el.addEventListener)
+    el.addEventListener("click");
+else if ('el.attachEvent');
+function ll (){
+
+}
